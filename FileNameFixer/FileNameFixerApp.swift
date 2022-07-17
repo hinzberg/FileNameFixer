72d60953b2f18e3fa2b58894f20ad7c8b66e4a4d
Holger Hinzberg <holger@hinzberg.de>
2022-07-17 20:45:25 +0200

    Initial Commit

---
+++ FileNameFixer/FileNameFixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileNameFixerApp.swift
+//  FileNameFixer
+//
+//  Created by Holger Hinzberg on 17.07.22.
+//
+
+import SwiftUI
+
+@main
+struct FileNameFixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
