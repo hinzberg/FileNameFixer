47d6a40ac7f212f934c35b50389df5004eb0f18f
Holger Hinzberg <holger@hinzberg.de>
2024-12-17 13:26:27 +0100

    2024-12-17 Inspector with file size and quicklook

--- FileNameFixer/InspectorView.swift
+++ FileNameFixer/InspectorView.swift
@@ -1,18 +1,39 @@
-//
 //  InspectorView.swift
 //  FileNameFixer
-//
 //  Created by Holger Hinzberg on 11.11.23.
-//
 
 import SwiftUI
+import QuickLook
 
 struct InspectorView: View {
     
     @Binding var fileInfo : FileInfo?
+    @State var documentUrl: URL?
     
     var body: some View {
-        Text("\(fileInfo?.destinationFileNameOnlyWithOutExtension ?? "Nothing")")
+        
+        VStack {
+            
+            if  fileInfo != nil {
+                Text(fileInfo!.currentFileNameOnly)
+                    .padding(4)
+                
+                Text(URL(fileURLWithPath: fileInfo!.currentFileNameWithPathAndExtension).fileSizeString )
+                    .padding(4)
+                
+                Button("Show Quicklook", action: {
+                    documentUrl = URL(fileURLWithPath: fileInfo!.currentFileNameWithPathAndExtension)
+                })
+                .padding(4)
+                .quickLookPreview($documentUrl)
+                
+                Spacer()
+                
+            } else {
+                Text("No Selection")
+                    .padding(4)
+            }
+        }
     }
 }
 
